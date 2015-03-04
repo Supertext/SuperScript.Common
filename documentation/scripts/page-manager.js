d4adc4c79d67bde28fe662f10874e3bc9afa29fe
Andrew Jameson <andrew_jameson@hotmail.com>
2015-03-04 21:10:22 +0100

    Update page-manager.js

--- documentation/scripts/page-manager.js
+++ documentation/scripts/page-manager.js
@@ -1,6 +1,6 @@
 var PageManager = function (win, doc, Sizzle) {
 
-	var self = this,
+    var self = this,
 		elmntContent = doc.getElementById("content"),
 		location = doc.location,
 		projectName = location.href.split("/")[3],
@@ -9,110 +9,126 @@ var PageManager = function (win, doc, Sizzle) {
 		removeLastSegment = urlSegments.pop(),
 		urlDirectory = location.protocol + "//" + location.host + urlSegments.join("/") + "/documentation/",
 		escapeRegExp = function (string) {
-    			return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+		    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 		},
 		replaceAll = function (string, find, replace) {
-			return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+		    return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 		},
-		createXhr = function() {
-			var xhr;
-			if (window.ActiveXObject) {
-				try {
-					xhr = new ActiveXObject("Microsoft.XMLHTTP");
-				}
-				catch(e) {
-					alert(e.message);
-					xhr = null;
-				}
-			}
-			else {
-				xhr = new XMLHttpRequest();
-			}
-			return xhr;
+		createXhr = function () {
+		    var xhr;
+		    if (window.ActiveXObject) {
+		        try {
+		            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+		        }
+		        catch (e) {
+		            alert(e.message);
+		            xhr = null;
+		        }
+		    }
+		    else {
+		        xhr = new XMLHttpRequest();
+		    }
+		    return xhr;
 		},
-		addClass = function(node, cls) {
-			if (typeof(node) === "undefined" || node === null) {
-				return;
-			}
-			if (Object.prototype.toString.call(node) === "[object Array]") {
-				for (var i = 0, iMax = node.length; i < iMax; i++) {
-					var n = node[i];
-					n.className = n.className + " " + cls;
-				}
-				return;
-			}
-			node.className = node.className + " " + cls;
+		addClass = function (node, cls) {
+		    if (typeof (node) === "undefined" || node === null) {
+		        return;
+		    }
+		    if (Object.prototype.toString.call(node) === "[object Array]") {
+		        for (var i = 0, iMax = node.length; i < iMax; i++) {
+		            var n = node[i];
+		            n.className = n.className + " " + cls;
+		        }
+		        return;
+		    }
+		    node.className = node.className + " " + cls;
 		},
-		removeClass = function(node, cls) {
-			if (typeof(node) === "undefined" || node === null) {
-				return;
-			}
-			var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
-			if (Object.prototype.toString.call(node) === "[object Array]") {
-				for (var i = 0, iMax = node.length; i < iMax; i++) {
-					n.className = n.className.replace(reg, " ");
-				}
-				return;
-			}
-			node.className = node.className.replace(reg, " ");
-		};
-	
-		configureRouting = function() {
-			Routing.map("#!/:contentName(/:anchor)")
-				.before(function() {
-					elmntContent.innerHTML = "<img src=\"https://assets-cdn.github.com/images/spinners/octocat-spinner-32.gif\" alt=\"loading...\" class=\"center-block\" />";
-					removeClass(Sizzle("li.active")[0], "active");
-				})
-				.to(function() {
-					var specificPath = replaceAll(this.params.contentName.value, ".", "/") + ".html",
-						anchor = this.params.anchor.value,
-						loadUrl = urlDirectory + specificPath,
-						elmntLink = Sizzle("a[href='#!/" + this.params.contentName.value + "']")[0],
-						xhr = createXhr();
+		removeClass = function (node, cls) {
+		    if (typeof (node) === "undefined" || node === null) {
+		        return;
+		    }
+		    var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+		    if (Object.prototype.toString.call(node) === "[object Array]") {
+		        for (var i = 0, iMax = node.length; i < iMax; i++) {
+		            n.className = n.className.replace(reg, " ");
+		        }
+		        return;
+		    }
+		    node.className = node.className.replace(reg, " ");
+		},
+        getParentOfType = function(currentNode, tagName, cls) {
+            if (typeof (currentNode) === "undefined" || currentNode === null) {
+                return null;
+            }
+            var p = currentNode.parentElement;
+            while (p !== null) {
+                var p = currentNode.parentElement;
+                if (p.tagName.toLowerCase === tagName) {
+                    if (typeof(cls) !== "undefined" && cls !== null && cls.length > 0 && (" " + p.className + " ").replace(/[\n\t]/g, " ").indexOf(" " + cls + " ") > -1) {
+                        return p;
+                    }
+                }
+                p = p.parentElement;
+            }
+            return null;
+        };
 
-					xhr.onreadystatechange = function() {
-						if (xhr.readyState === 4 && xhr.status == 200) {
-							elmntContent.innerHTML = xhr.responseText;
+    configureRouting = function() {
+        Routing.map("#!/:contentName(/:anchor)")
+            .before(function() {
+                elmntContent.innerHTML = "<img src=\"https://assets-cdn.github.com/images/spinners/octocat-spinner-32.gif\" alt=\"loading...\" class=\"center-block\" />";
+                removeClass(Sizzle("li.active")[0], "active");
+            })
+            .to(function() {
+                var specificPath = replaceAll(this.params.contentName.value, ".", "/") + ".html",
+                    anchor = this.params.anchor.value,
+                    loadUrl = urlDirectory + specificPath,
+                    elmntLink = Sizzle("a[href='#!/" + this.params.contentName.value + "']"),
+                    xhr = createXhr();
 
-							SyntaxHighlighter.highlight();
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4 && xhr.status == 200) {
+                        elmntContent.innerHTML = xhr.responseText;
 
-							if (typeof(anchor) !== "undefined") {
-								var elmntAnchor = doc.getElementById(anchor);
-								if (elmntAnchor !== null) {
-									elmntAnchor.scrollIntoView(true);
-								}
-							}
-						} else {
-							elmntContent.innerHTML = "<p>Sorry, it looks like an error occurred!</p><p>How about letting us know by creating an <a href=\"" + issueUrl + "\" target=\"_blank\">issue</a>?</p>"
-						}
-					}
-					xhr.open('GET', loadUrl, true);
-					xhr.setRequestHeader("Content-type", "application/x-www-form-urlencode");
-					xhr.send();
+                        SyntaxHighlighter.highlight();
 
-					elmntContent.className = elmntContent.className + " " + this.params.contentName.value;
+                        if (typeof (anchor) !== "undefined") {
+                            var elmntAnchor = doc.getElementById(anchor);
+                            if (elmntAnchor !== null) {
+                                elmntAnchor.scrollIntoView(true);
+                            }
+                        }
+                    } else {
+                        elmntContent.innerHTML = "<p>Sorry, it looks like an error occurred!</p><p>How about letting us know by creating an <a href=\"" + issueUrl + "\" target=\"_blank\">issue</a>?</p>"
+                    }
+                }
+                xhr.open('GET', loadUrl, true);
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencode");
+                xhr.send();
 
-					if (elmntLink.length > 0) {
-						var elmntLi = elmntLink.parent("li");
-						addClass(elmntLi, "active");
-						elmntLi.parents("li.dropdown").addClass("active");
-					}
-				});
-		};
+                elmntContent.className = elmntContent.className + " " + this.params.contentName.value;
 
-	return {
-		"Init": function() {
-			configureRouting();
-		}
-	};
+                if (typeof (elmntLink) !== "undefined") {
+                    var elmntLi = getParentOfType(elmntLink, "li");
+                    addClass(elmntLi, "active");
+                    getParentOfType(elmntLi, "li", "dropdown").addClass("active");
+                }
+            });
+    };
+
+    return {
+        "Init": function () {
+            configureRouting();
+        }
+    };
 }(window, window.document, window.Sizzle);
 
 $(function() {
-	PageManager.Init();
-	Routing.root("#!/index");
-	Routing.listen();
-	
-	SyntaxHighlighter.defaults["gutter"] = false;
-	SyntaxHighlighter.defaults["toolbar"] = false;
-	SyntaxHighlighter.all();
+    PageManager.Init();
+    Routing.root("#!/index");
+    Routing.listen();
+
+    SyntaxHighlighter.defaults["gutter"] = false;
+    SyntaxHighlighter.defaults["toolbar"] = false;
+    SyntaxHighlighter.all();
 });
