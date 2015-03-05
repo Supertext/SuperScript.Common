955a09592e9059b00ebe5e00d2b9392ffaea121d
Andrew Jameson <andrew_jameson@hotmail.com>
2015-03-05 21:46:04 +0100

    Update page-manager.js

--- documentation/scripts/page-manager.js
+++ documentation/scripts/page-manager.js
@@ -1,75 +1,80 @@
 var PageManager = function (win, doc, Sizzle) {
 
     var self = this,
-		elmntContent = doc.getElementById("content"),
-		location = doc.location,
-		projectName = location.href.split("/")[3],
-		issueUrl = "https://github.com/Supertext/" + projectName + "/issues/new",
-		urlSegments = location.pathname.split("/"),
-		removeLastSegment = urlSegments.pop(),
-		urlDirectory = location.protocol + "//" + location.host + urlSegments.join("/") + "/documentation/",
-		escapeRegExp = function (string) {
-		    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-		},
-		replaceAll = function (string, find, replace) {
-		    return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-		},
-		createXhr = function () {
-		    var xhr;
-		    if (window.ActiveXObject) {
-		        try {
-		            xhr = new ActiveXObject("Microsoft.XMLHTTP");
-		        }
-		        catch (e) {
-		            alert(e.message);
-		            xhr = null;
-		        }
-		    }
-		    else {
-		        xhr = new XMLHttpRequest();
-		    }
-		    return xhr;
-		},
-		addClass = function (node, cls) {
-		    if (typeof (node) === "undefined" || node === null) {
-		        return;
-		    }
-		    if (Object.prototype.toString.call(node) === "[object Array]") {
-		        for (var i = 0, iMax = node.length; i < iMax; i++) {
-		            var n = node[i];
-		            n.className = n.className + " " + cls;
-		        }
-		        return;
-		    }
-		    node.className = node.className + " " + cls;
-		},
-		removeClass = function (node, cls) {
-		    if (typeof (node) === "undefined" || node === null) {
-		        return;
-		    }
-		    var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
-		    if (Object.prototype.toString.call(node) === "[object Array]") {
-		        for (var i = 0, iMax = node.length; i < iMax; i++) {
-		            n.className = n.className.replace(reg, " ");
-		        }
-		        return;
-		    }
-		    node.className = node.className.replace(reg, " ");
-		},
-        getParentOfType = function (currentNode, tagName, cls) {
+        elmntContent = doc.getElementById("content"),
+        location = doc.location,
+        projectName = location.href.split("/")[3],
+        issueUrl = "https://github.com/Supertext/" + projectName + "/issues/new",
+        urlSegments = location.pathname.split("/"),
+        removeLastSegment = urlSegments.pop(),
+        urlDirectory = location.protocol + "//" + location.host + urlSegments.join("/") + "/documentation/",
+        escapeRegExp = function(string) {
+            return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+        },
+        replaceAll = function(string, find, replace) {
+            return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+        },
+        createXhr = function() {
+            var xhr;
+            if (window.ActiveXObject) {
+                try {
+                    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {
+                    alert(e.message);
+                    xhr = null;
+                }
+            } else {
+                xhr = new XMLHttpRequest();
+            }
+            return xhr;
+        },
+        addClass = function(node, cls) {
+            if (typeof (node) === "undefined" || node === null) {
+                return;
+            }
+            if (Object.prototype.toString.call(node) === "[object Array]") {
+                for (var i = 0,
+                         iMax = node.length; i < iMax; i++) {
+                    var n = node[i];
+                    n.className = n.className + " " + cls;
+                }
+                return;
+            }
+            node.className = node.className + " " + cls;
+        },
+        removeClass = function(node, cls) {
+            if (typeof (node) === "undefined" || node === null) {
+                return;
+            }
+            var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+            if (Object.prototype.toString.call(node) === "[object Array]") {
+                for (var i = 0,
+                         iMax = node.length; i < iMax; i++) {
+                    n.className = n.className.replace(reg, " ");
+                }
+                return;
+            }
+            node.className = node.className.replace(reg, " ");
+        },
+        getParentOfType = function(currentNode, tagName, cls) {
             if (typeof (currentNode) === "undefined" || currentNode === null) {
                 return null;
             }
-            var p = currentNode.parentElement;
+            var p = currentNode.parentElement,
+                tagName = tagName.toLowerCase;
             while (typeof (p) !== "undefined" && p !== null) {
-                if (p.tagName.toLowerCase === tagName) {
-                    if (typeof (cls) !== "undefined" && cls !== null && cls.length > 0 && (" " + p.className + " ").replace(/[\n\t]/g, " ").indexOf(" " + cls + " ") > -1) {
-                        return p;
-                    } else {
+                if (p.tagName.toLowerCase !== tagName) {
+                    p = p.parentElement;
+                    continue;
+                }
+                if (typeof (cls) !== "undefined" && cls !== null && cls.length > 0) {
+                    if ((" " + p.className + " ").replace(/[\n\t]/g, " ").indexOf(" " + cls + " ") > -1) {
                         return p;
                     }
+                    p = p.parentElement;
+                } else {
+                    return p;
                 }
-                p = p.parentElement;
             }
             return null;
         };
@@ -124,7 +129,7 @@ var PageManager = function (win, doc, Sizzle) {
     };
 }(window, window.document, window.Sizzle);
 
-$(function () {
+$(function() {
     PageManager.Init();
     Routing.root("#!/index");
     Routing.listen();
