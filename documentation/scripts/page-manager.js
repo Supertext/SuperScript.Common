8721351bc00ad60ec429c66e3f4e6dff026a5721
Andrew Jameson <andrew_jameson@hotmail.com>
2015-03-23 19:14:24 +0100

    Update page-manager.js

--- documentation/scripts/page-manager.js
+++ documentation/scripts/page-manager.js
@@ -49,7 +49,7 @@ var PageManager = function (win, doc, $) {
                     }
                 });
 
-                elmntContent.className = elmntContent.className + " " + this.params.contentName.value;
+                elmntContent.className = "col-xs-12 " + this.params.contentName.value;
 
                 if (elmntLink.length > 0) {
                     var elmntLi = elmntLink.parent("li");
